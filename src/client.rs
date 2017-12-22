4b0ee57c377d0062b199c96534c8cd47f7612a67
Chen Weiguang <chen.weiguang@gmail.com>
2017-12-22 23:24:27 +0800

    Added metadata function.

--- src/client.rs
+++ src/client.rs
@@ -2,6 +2,8 @@ use super::error::Result;
 
 use std::io::Write;
 use std::net::{TcpStream, ToSocketAddrs};
+use std::path::Path;
+use std::time::SystemTime;
 
 const RPC_VERSION: u8 = 0x09;
 const SERVICE_CLASS: u8 = 0x0;
@@ -14,6 +16,15 @@ const STANDBY_EXCEPTION_CLASS: &str = "org.apache.hadoop.ipc.StandbyException";
 pub struct Client {
     stream: TcpStream,
 }
+
+#[derive(Clone, Debug)]
+pub struct Metadata;
+
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
+pub struct FileType;
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct Permissions;
 
 impl Client {
     fn new<A: ToSocketAddrs>(addr: A) -> Result<Client> {
@@ -32,6 +43,68 @@ impl Client {
         stream.write_all(&rpc_header)?;
 
         Ok(Client { stream: stream })
+    }
+
+    fn metadata<P: AsRef<Path>>(path: P) -> Result<Metadata> {
+        unimplemented!()
+    }
+}
+
+impl Metadata {
+    pub fn file_type(&self) -> FileType {
+        unimplemented!()
+    }
+
+    pub fn is_dir(&self) -> bool {
+        self.file_type().is_dir()
+    }
+
+    pub fn is_file(&self) -> bool {
+        self.file_type().is_file()
+    }
+
+    pub fn len(&self) -> u64 {
+        unimplemented!()
+    }
+
+    pub fn permissions(&self) -> Permissions {
+        unimplemented!()
+    }
+
+    pub fn modified(&self) -> Result<SystemTime> {
+        unimplemented!()
+    }
+
+    pub fn accessed(&self) -> Result<SystemTime> {
+        unimplemented!()
+    }
+
+    pub fn created(&self) -> Result<SystemTime> {
+        unimplemented!()
+    }
+}
+
+impl FileType {
+    pub fn is_dir(&self) -> bool {
+        unimplemented!()
+    }
+
+    pub fn is_file(&self) -> bool {
+        unimplemented!()
+    }
+
+    pub fn is_symlink(&self) -> bool {
+        unimplemented!()
+    }
+}
+
+impl Permissions {
+    pub fn readonly(&self) -> bool {
+        unimplemented!()
+    }
+
+    pub fn set_readonly(&mut self, readonly: bool) {
+        unimplemented!()
     }
 }
 
